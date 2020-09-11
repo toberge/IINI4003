4b9025dc192221c3ae7b55efbf722f5886e997c7
toberge <toberge@stud.ntnu.no>
2020-09-11 21:37:04 +0200

    Add CMakeLists.txt to all assignments

--- 04_containers_lambda/tardis.sh
+++ 04_containers_lambda/tardis.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-tar czf øving4.tar.gz vector_demo.cpp name_concatenator/{CMakeLists.txt,main.cpp,run,mkdebug.sh}
+tar czf øving4.tar.gz -- */CMakeLists.txt vector_demo/vector_demo.cpp \
+    name_concatenator/{Makefile,main.cpp,run,mkdebug.sh}
--- tar.sh
+++ tar.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-tar czf "${1:-submission}".tar.gz -- *[^g][^z]
+# find . ! \( -name build -name "*.gz" \) \
+#     -exec tar czf "${1:-submission}".tar.gz -- {} +
+tar --exclude-vcs-ignores -czf "${1:-submission}".tar.gz -- *
